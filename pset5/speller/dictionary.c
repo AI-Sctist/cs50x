d1458e256464602da608e33014102310d303d7e5
AI-Sctist <giahuythcs2008@gmail.com>
2025-08-02 12:44:07 +0700

    refactor(pset5/speller): rewrite the program and fix its bug

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -15,70 +15,24 @@ typedef struct node
     struct node *next;
 } node;
 
-// TODO: Choose number of buckets in hash table _ 26^5
-const unsigned int SLOTS = 11881376;
+// TODO: Choose number of buckets in hash table_(26^5)
+const unsigned int BUCKET_COUNT = 11881376;
 
-// Return value for unsigned in size() function
-unsigned int COUNT_WORDS = 0;
+// Return value for size() function
+unsigned int WORD_COUNT = 0;
 
 // Hash table
-node *table[SLOTS];
+node *table[BUCKET_COUNT];
 
-bool make_node(unsigned int bucket, const char *word)
-{
-    node *new_node = malloc(sizeof(node));
-    if (new_node == NULL)
-    {
-        return false;
-    }
-    else
-    {
-        COUNT_WORDS++;
-    }
-
-    strcpy(new_node->word, word);
-
-    if (table[bucket] == NULL)
-    {
-        new_node->next = NULL;
-        table[bucket] = new_node;
-    }
-    else
-    {
-        new_node->next = table[bucket];
-        table[bucket] = new_node;
-    }
-
-    // Successful making new node
-    return true;
-}
-
-int compare(const char *str1, const char *str2)
-{
-    int l1 = strlen(str1);
-    int l2 = strlen(str2);
-    int mn = (l1 < l2) ? l1 : l2;
-
-    for (int i = 0; i < mn; i++)
-    {
-        if (tolower(*(str1 + i)) < *(str2 + i))
-        {
-            return -1;
-        }
-        else if (tolower(*(str1 + i)) > *(str2 + i))
-        {
-            return 1;
-        }
-    }
-
-    return (l1 == l2) ? 0 : ((l1 < l2) ? -1 : 1);
-}
+// Helper functions
+int compare(const char *s1, const char *s2);
+bool append(unsigned int hash_code, const char *word);
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    unsigned int bucket = hash(word);
-    node *ptr = table[bucket];
+    unsigned int hash_code = hash(word);
+    node *ptr = table[hash_code];
 
     while (ptr != NULL)
     {
@@ -88,15 +42,10 @@ bool check(const char *word)
             return true;
         }
 
-        /**
-         * Look at the way we create the hash table: Take words from the dictionary
-         * file that are in increasing order. Whenever taking a new word, insert it
-         * at the beginning of the linked-list in a bucket.
-         *
-         * The order of words in buckets is decreasing order, so if the current word
-         * is smaller than the given word, there's no hope for the remaining words to
-         * be equal to the given word!
-         */
+        // Branch and bound: since the linked list in a bucket is in decreasing order,
+        // if the current comparing word is smaller than the given word, then its
+        // following words must also be smaller than the given word, so they cannot be
+        // equal to the given word.
         if (result > 0)
         {
             return false;
@@ -111,54 +60,62 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    unsigned int bucket = 0;
-    unsigned int len = strlen(word);
+    unsigned int hash_code = 0;
 
-    for (int i = 0; i < 5; i++)
+    int index = 0;
+    int len = strlen(word);
+
+    for (int i = 0; i < 5; i++, index++)
     {
-        bucket = bucket * 26;
-        if (i < len && isalpha(*(word + i)))
+        while (index < len && word[index] == '\'')
         {
-            bucket += tolower(*(word + i)) - 'a';
+            index++;
+        }
+
+        hash_code *= 26;
+        if (index < len)
+        {
+            hash_code += toupper(word[index]) - 'A';
         }
     }
-    return bucket;
+    return hash_code;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    for (int i = 0; i < SLOTS; i++)
+    for (int i = 0; i < BUCKET_COUNT; i++)
     {
         table[i] = NULL;
     }
 
-    FILE *file = fopen(dictionary, "r");
-    if (file == NULL)
+    FILE *dict = fopen(dictionary, "r");
+    if (dict == NULL)
     {
         return false;
     }
 
-    int index = 0;
-    char word[LENGTH + 1];
+    // Add two bytes for the newline char ('\n') and the null terminator ('\0')
+    char word[LENGTH + 2];
 
-    while (fread(&word[index], sizeof(char), 1, file))
+    while (fgets(word, LENGTH + 1, dict) != NULL)
     {
-        if (word[index] != '\n')
-        {
-            index++;
-        }
-        else
-        {
-            word[index] = '\0';
-            make_node(hash(word), word);
+        // Remove the newline char at the end
+        word[strcspn(word, "\n")] = '\0';
 
-            // Reset for new word
-            index = 0;
+        // strlen(word) == 0 means it's at the end of file (blank line), my bad :((
+        if (strlen(word) > 0)
+        {
+            bool success = append(hash(word), word);
+            if (!success)
+            {
+                fclose(dict);
+                return false;
+            }
         }
     }
 
-    fclose(file);
+    fclose(dict);
 
     // Successfull loading
     return true;
@@ -167,13 +124,13 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    return COUNT_WORDS;
+    return WORD_COUNT;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    for (int i = 0; i < SLOTS; i++)
+    for (int i = 0; i < BUCKET_COUNT; i++)
     {
         node *ptr = table[i];
 
@@ -186,5 +143,49 @@ bool unload(void)
     }
 
     // Success unloading hash table
+    return true;
+}
+
+//============================================================//
+//                      HELPER FUNCTIONS                      //
+//============================================================//
+
+// Returns -1 if s1 < s2, 0 if s1 == s2, otherwise returns 1
+int compare(const char *s1, const char *s2)
+{
+    int l1 = strlen(s1);
+    int l2 = strlen(s2);
+    int lim = (l1 < l2) ? l1 : l2;
+
+    for (int i = 0; i < lim; i++)
+    {
+        if (tolower(s1[i]) < tolower(s2[i]))
+        {
+            return -1;
+        }
+        else if (tolower(s1[i]) > tolower(s2[i]))
+        {
+            return 1;
+        }
+    }
+    return (l1 == l2) ? 0 : ((l1 < l2) ? -1 : 1);
+}
+
+// Append a new word to the hash table
+bool append(unsigned int code, const char *word)
+{
+    node *new_node = malloc(sizeof(node));
+    if (new_node == NULL)
+    {
+        return false;
+    }
+
+    strcpy(new_node->word, word);
+    new_node->next = (table[code] == NULL) ? NULL : table[code];
+
+    WORD_COUNT++;
+    table[code] = new_node;
+
+    // Successfull appending
     return true;
 }
