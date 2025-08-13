b0a4a34f8086b15aaa6191cbd94584670468d025
AI-Sctist <giahuythcs2008@gmail.com>
2025-08-13 15:30:51 +0700

    refactor(additional-practice/figlet): rewrite code while learning CS50 Python"

--- additional-practice/figlet/figlet.py
+++ additional-practice/figlet/figlet.py
@@ -2,51 +2,46 @@ import pyfiglet
 import random
 import sys
 
-# Initialize figlet object
-figlet = pyfiglet.Figlet()
+# Create an object to access the entire method, including setting fonts and rendering text
+FIGLET = pyfiglet.Figlet()
 
-# Get available fonts
-available_font = pyfiglet.FigletFont.getFonts()
+AVAILABLE_FONTS = pyfiglet.FigletFont.getFonts()
+
+
+def random_font(text):
+    random_font = random.choice(AVAILABLE_FONTS)
+
+    FIGLET.setFont(font=random_font)
+    print(FIGLET.renderText(text))
+
+
+def optional_font(text, optional_font):
+    FIGLET.setFont(font=optional_font)
+    print(FIGLET.renderText(text))
 
 
 def main():
+
+    # User choice: render text with a random font
     if len(sys.argv) == 1:
-        text = input("Text: ")
-        random_choice(text)
-    elif error():
-        print("Invalid usage")
-        sys.exit(1)
+
+        text = input("Input: ")
+        random_font(text)
+
+    # User choice: render text with an optional font
+    elif len(sys.argv) == 3:
+
+        # Check if command is incorrect and guide user how to do
+        if sys.argv[1] not in ["-f", "--font"]:
+            sys.exit("OPTION: -f or --font")
+        if sys.argv[2] not in AVAILABLE_FONTS:
+            sys.exit("Font is not available")
+
+        text = input("Input: ")
+        optional_font(text, sys.argv[2])
+
     else:
-        text = input("Text: ")
-        optional_choice(text)
-
-
-def random_choice(text):
-    # Set a random font
-    random_font = random.choice(available_font)
-    figlet.setFont(font=random_font)
-
-    # Display
-    print(figlet.renderText(text))
-
-
-def error():
-    if len(sys.argv) != 3:
-        return True
-
-    if sys.argv[1] not in ["-f", "--font"]:
-        return True
-
-    try:
-        figlet.setFont(font=sys.argv[2])
-    except pyfiglet.FontNotFound:
-        return True
-
-    return False
-
-
-def optional_choice(text):
-    print(figlet.renderText(text))
+        sys.exit("Usage: python figlet.py [OPTION + FONT]")
 
 
 if __name__ == "__main__":
